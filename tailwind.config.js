3652cb29e93957fe20a8fa8b9811c76851c92c6a
Taofeek Lateef <lateef4ril32@gmail.com>
2026-02-06 23:11:49 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'border-border'
+  ],
   theme: {
     extend: {
       colors: {
